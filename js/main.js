682917504e9ed52475109dce7061fc1311643db6
andalons <andreacor001@gmail.com>
2022-03-29 16:21:30 +0200

    add all js functionalities

--- js/main.js
+++ js/main.js
@@ -1,64 +1,79 @@
 "use strict";
 const playBtn = document.querySelector('.js_play_btn');
+const restartBtn = document.querySelector('.js_restart_btn');
 const message = document.querySelector('.js_message');
-const remainingAmount = document.querySelector('.js_remaining_amount');
+const balance = document.querySelector('.js_balance');
+let initialAmount = 50;
+balance.innerHTML= `${initialAmount}`;
+
 
 function getRandomNumber(max) {
-     //para comprobar qué número sale
     return Math.ceil(Math.random() * max);
     ;
   }
 
-function compareNumbers(){
-const aleatNum = getRandomNumber(6);
-const bettingNum = parseInt(document.querySelector('.js_betting_number').value);
-console.log(bettingNum);
-console.log(aleatNum);
-if (aleatNum === bettingNum) {
-    message.innerHTML = "Has ganado el doble de lo apostado";
-    return true; //no funciona
-} else {
-    message.innerHTML = "Has perdido lo apostado";
-    return false; //no funciona
+function play(aleatNum, bettingNum, bettingAmount, initialAmount){
+console.log(aleatNum, bettingNum, bettingAmount, initialAmount);
+
+if (initialAmount < bettingAmount) {
+    message.innerHTML = "No dispones de tanto saldo... Reajusta tu apuesta!"
+    return initialAmount;
 }
+if (bettingNum === 0) {
+    message.innerHTML = "No has elegido número... Reajusta tu apuesta!"
+    return initialAmount;
 }
 
-function calculateRemainingAmount () {
-    const bettingAmount = parseInt(document.querySelector('.js_betting_amount').value);
-    console.log (bettingAmount);
-    const remainingAmount = parseInt(document.querySelector('.js_remaining_amount').innerHTML);
-    let newRemainingAmount = 0;
-    console.log (remainingAmount);
-    const numberMatch = compareNumbers();
-       if (numberMatch === true) {
-           console.log (bettingAmount);
-            newRemainingAmount = (remainingAmount - bettingAmount)*2;
-            return newRemainingAmount;
-       } else {
-            newRemainingAmount = remainingAmount - bettingAmount*2;
-            return newRemainingAmount;
-       } 
-       remainingAmount.innerHTML = `${newRemainingAmount}`
-       
-      
-    }
-/* function paintHtml(){
-    const numberMatch = compareNumbers();
-    if (numberMatch === true) {
+if (initialAmount > 0 && initialAmount < 200) {
+    const newAmount = initialAmount-bettingAmount;
+    console.log(newAmount);
+    if (aleatNum === bettingNum) {
         message.innerHTML = "Has ganado el doble de lo apostado";
-    
-    } else { 
-        
-        
+        initialAmount = newAmount + (bettingAmount*2);
+        console.log(`ganas! ahora tienes ${initialAmount}`);
+    } else {
+        message.innerHTML = "Has perdido lo apostado";
+        initialAmount = newAmount;
+        console.log(`pierdes! ahora tienes ${initialAmount}`);
     }
-} */
-
-
-function handleClick (event){
-    event.preventDefault();
-    compareNumbers (); 
-   /*  paintHtml ();  */ 
-    calculateRemainingAmount ();
 }
 
-playBtn.addEventListener('click', handleClick);
+if (initialAmount <= 0){
+    message.innerHTML = "¡HAS PERDIDO!"
+    playBtn.classList.add ('collapsed');
+    restartBtn.classList.remove('collapsed');
+}
+if (initialAmount >= 60) {
+    message.innerHTML = "¡HAS GANADO!"
+    playBtn.classList.add ('collapsed');
+    restartBtn.classList.remove('collapsed');
+}
+
+return initialAmount;
+}
+
+
+function handleClickPlay (event){
+    event.preventDefault();
+    const aleatNum = getRandomNumber(6);
+    const bettingNum = parseInt(document.querySelector('.js_betting_number').value);
+    const bettingAmount = parseInt(document.querySelector('.js_betting_amount').value);
+    initialAmount = play (aleatNum, bettingNum, bettingAmount, initialAmount); 
+    balance.innerHTML= `${initialAmount}`;
+   /*  paintHtml ();  */ 
+   /*  calculateAmounts (); */
+}
+
+function handleClickRestart (event) {
+    event.preventDefault();
+    initialAmount = 50;
+    balance.innerHTML= `${initialAmount}`;
+    playBtn.classList.remove ('collapsed');
+    restartBtn.classList.add('collapsed');
+    message.innerHTML = "Vamos a intentarlo de nuevo!";
+    
+}
+
+playBtn.addEventListener('click', handleClickPlay);
+
+restartBtn.addEventListener('click', handleClickRestart);
